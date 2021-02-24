4ec14e9a055c3ca0cddf8771a58ef10578a04072
yund7 <762295440@qq.com>
2021-02-24 20:14:22 +0800

    1

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+layui.use(["element", "laypage"], () => {
+  let element = layui.element
+  let laypage = layui.laypage
+  const $ = layui.$
+  
+  element.tabDelete('demo', 'xxx')
+ 
+  laypage.render({
+    elem: "laypage",
+    count: $("#laypage").data("maxnum"),
+    limit: $("#laypage").data("limit"),
+    groups: 3,
+    curr: location.pathname.replace("/page/", ""),
+    jump(obj, f){
+      $("#laypage a").each((i, v) => {
+        let pageValue = `/page/${$(v).data("page")}`
+        v.href = pageValue
+      })
+    }
+  })
+})
